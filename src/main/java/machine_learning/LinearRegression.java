25d59b631841bc141b74867d3795aaf5f766e014
Haizhou Fu <haizhou@uber.com>
2016-09-28 22:55:47 -0700

    fix

--- src/main/java/machine_learning/LinearRegression.java
+++ src/main/java/machine_learning/LinearRegression.java
@@ -4,7 +4,9 @@ import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.stat.descriptive.moment.Mean;
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+import org.ejml.simple.SimpleMatrix;
 
+import java.util.Random;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
@@ -12,8 +14,10 @@ public class LinearRegression {
 
     private static final StandardDeviation standardDeviation = new StandardDeviation();
     private static final Mean mean = new Mean();
+    private static final Random random = new Random(System.currentTimeMillis());
 
-    private static final double ALPHA = 0.0001;
+    private static final double ALPHA = 0.00001;
+    private static final double DELTA = 0.01;
 
     static double [] scaleFeature(double [] featureVector) {
         final double meanVal = mean.evaluate(featureVector);
@@ -35,40 +39,71 @@ public class LinearRegression {
     }
 
     static double descent(int ithDimension, double [] thetaVector, double [][] exampleMatrix, double [] labelVector) {
-        if(exampleMatrix.length != labelVector.length)
-            throw new RuntimeException("Size of the example and the label has to be the same!");
-
-        if(exampleMatrix[0].length != thetaVector.length)
-            throw new RuntimeException("Size of the theta vector and the dimension of the output has to be the same!");
 
         double theta = thetaVector[ithDimension];
-        double sum = 0;
-        for(int i = 0; i < exampleMatrix.length; i++) {
-            double [] ithExampleVector = exampleMatrix[i]; // ith example
-            double ithLabel = labelVector[i]; // ith actual output
+        double avg = IntStream.range(0, exampleMatrix.length)
+            .parallel()
+            .mapToDouble(i -> {
+                double [] ithExampleVector = exampleMatrix[i]; // ith example
+                double ithLabel = labelVector[i]; // ith actual output
+                double yHead = hypothesis(thetaVector, ithExampleVector);
+                return (yHead - ithLabel) * ithExampleVector[ithDimension];
+            })
+            .average()
+            .getAsDouble();
 
-            double yHead = IntStream.range(0, ithExampleVector.length)
-                .parallel()
-                .mapToDouble(j -> thetaVector[j] * ithExampleVector[j])
-                .sum();
-            sum += (yHead - ithLabel) * ithExampleVector[ithDimension];
-        }
-        double avg = sum / exampleMatrix.length;
         return theta - ALPHA * avg;
     }
 
+    static double hypothesis(double [] thetaVector, double [] exampleVector) {
+        return IntStream.range(0, exampleVector.length)
+            .parallel()
+            .mapToDouble(i -> thetaVector[i] * exampleVector[i])
+            .sum();
+    }
+
+    public static double costFunction(double [] thetaVector, double [][] exampleMatrix, double [] labelVector) {
+        double totalSquareErrSum = 0;
+        for(int i = 0; i < exampleMatrix.length; i++) {
+            double [] ithExampleVector = exampleMatrix[i]; // ith example
+            double ithLabel = labelVector[i]; // ith actual output
+            double yHead = hypothesis(thetaVector, ithExampleVector);
+            double err = yHead - ithLabel;
+            totalSquareErrSum += err * err;
+        }
+        return totalSquareErrSum / labelVector.length / 2;
+    }
+
+    /**
+     * Gradient Descent Algorithm
+     * @param exampleMatrix
+     * @param labelVector
+     * @return
+     */
     public static double [] gradientDescent(double [][] exampleMatrix, double [] labelVector) {
+        if(exampleMatrix.length == 0)
+            throw new RuntimeException("No examples as input!");
+
+        if(exampleMatrix.length != labelVector.length)
+            throw new RuntimeException("Size of the example and the dimension of the label vector has to be the same!");
 
         final double [][] scaledExampleMatrix = scaleExamples(exampleMatrix);
-        boolean isConverged = false;
+
         double [] thetaVector = new double[exampleMatrix[0].length];
-        while(!isConverged) {
+        for(int i = 0; i < thetaVector.length; i++)
+            thetaVector[i] = random.nextDouble();
+        double cost = costFunction(thetaVector, exampleMatrix, labelVector);
+        while(true) {
             final double [] finalThetaVectorCopy = thetaVector;
             double[] newThetaVector = IntStream.range(0, thetaVector.length)
                 .parallel()
                 .mapToDouble(i -> descent(i, finalThetaVectorCopy, scaledExampleMatrix, labelVector))
                 .toArray();
-            isConverged = isConverged(thetaVector, newThetaVector);
+            double newCost = costFunction(newThetaVector, exampleMatrix, labelVector);
+            if(cost - newCost < DELTA)
+                break;
+            cost = newCost;
+            System.out.println(cost);
             thetaVector = newThetaVector;
         }
         return thetaVector;
@@ -76,9 +111,34 @@ public class LinearRegression {
 
     static boolean isConverged(double [] T, double [] newT) {
         for(int i = 0; i < T.length; i++) {
-            if(Math.abs(newT[i] - T[i]) > ALPHA)
+            if(Math.abs(newT[i] - T[i]) > 0.0001)
                 return false;
         }
         return true;
+    }
+
+    /**
+     * Normal equation
+     * ((X^T)(X))^-1)(X^T)Y
+     * @param exampleMatrix  (N + 1) * M matrix, where N is the number of features, M is the number of examples
+     * @param labelVector  M * 1 matrix, where M is the number of examples
+     * @return
+     */
+    public static double [] normalEquation(double [][] exampleMatrix, double [][] labelVector) {
+        if(exampleMatrix.length == 0)
+            throw new RuntimeException("No examples as input!");
+
+        if(exampleMatrix.length != labelVector.length)
+            throw new RuntimeException("Size of the example and the dimension of the label vector has to be the same!");
+
+        if(1 != labelVector[0].length)
+            throw new RuntimeException("The label vector has to be one column!");
+
+        SimpleMatrix X = new SimpleMatrix(exampleMatrix);
+        SimpleMatrix Y = new SimpleMatrix(labelVector);
+
+        SimpleMatrix XT = X.transpose();
+        SimpleMatrix theta = XT.mult(X).invert().mult(XT).mult(Y);
+        return theta.getMatrix().getData();
     }
 }
--- src/test/java/machine_learning/TestLinearRegression.java
+++ src/test/java/machine_learning/TestLinearRegression.java
@@ -7,13 +7,70 @@ public class TestLinearRegression {
 
     @Test
     public void test1() {
-        double [][] exampleMatrix = {{1, 2104, 5, 1, 45},
-            {1, 1416, 3, 2, 40},
-            {1, 1534, 3, 2, 30},
-            {1, 852, 2, 1, 36},
-            {1, 3000, 4, 1, 38}};
+        double [][] exampleMatrix =
+            {{1, 2104, 5, 1, 45},
+             {1, 1416, 3, 2, 40},
+             {1, 1534, 3, 2, 30},
+             {1, 852, 2, 1, 36},
+             {1, 3000, 4, 1, 38}};
         double [] labelVector = {460, 232, 315, 178, 540};
         double [] thetaVector = LinearRegression.gradientDescent(exampleMatrix, labelVector);
-        Assert.assertEquals(thetaVector, new double[]{1.0, 2.0});
+        double [][] labelVectorCol = {{460}, {232}, {315}, {178}, {540}};
+        double [] thetaVectorNE = LinearRegression.normalEquation(exampleMatrix, labelVectorCol);
+        double cost1 = LinearRegression.costFunction(thetaVector, exampleMatrix, labelVector);
+        double cost2 = LinearRegression.costFunction(thetaVectorNE, exampleMatrix, labelVector);
+        //Assert.assertEquals(thetaVector, new double[]{1.0, 2.0});
+    }
+
+    @Test
+    public void test2() {
+        double [][] exampleMatrix =
+            {{1, 2104, 3},
+                {1, 1600, 3},
+                {1, 2400, 3},
+                {1, 1416, 2},
+                {1, 3000, 4},
+                {1, 1985, 4},
+                {1, 1534, 3},
+                {1, 1427, 3},
+                {1, 1380, 3},
+                {1, 1494, 3}};
+        double [] labelVector = {399900,
+            329900,
+            369000,
+            232000,
+            539900,
+            299900,
+            314900,
+            198999,
+            212000,
+            242500};
+        double [] thetaVector = LinearRegression.gradientDescent(exampleMatrix, labelVector);
+        double [][] labelVectorCol = {{399900},
+            {329900},
+            {369000},
+            {232000},
+            {539900},
+            {299900},
+            {314900},
+            {198999},
+            {212000},
+            {242500}};
+        double [] thetaVectorNE = LinearRegression.normalEquation(exampleMatrix, labelVectorCol);
+        double cost1 = LinearRegression.costFunction(thetaVector, exampleMatrix, labelVector);
+        double cost2 = LinearRegression.costFunction(thetaVectorNE, exampleMatrix, labelVector);
+    }
+
+    @Test
+    public void testCostFunction() {
+        double [][] exampleMatrix =
+            {{1, 3},
+             {1, 2},
+             {1, 4},
+             {1, 0}};
+        double [] labelVector = {4, 1, 3, 1};
+        double [] thetaVector = {0, 1};
+        double cost = LinearRegression.costFunction(thetaVector, exampleMatrix, labelVector);
+        Assert.assertEquals(cost, 0.5);
     }
 }
