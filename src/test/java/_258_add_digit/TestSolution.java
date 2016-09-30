9a15864d6fac0da113dcbd6eb38e34b9abe40091
Haizhou Fu <haizhou@uber.com>
2016-09-30 09:23:24 -0700

    digit

---
+++ src/test/java/_258_add_digit/TestSolution.java
@@ -0,0 +1,30 @@
+package _258_add_digit;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSolution {
+
+    Solution solution = new Solution();
+
+    @Test
+    public void test() {
+        Assert.assertEquals(solution.addDigits(1), 1);
+        Assert.assertEquals(solution.addDigits(11), 2);
+        Assert.assertEquals(solution.addDigits(99), 9);
+        Assert.assertEquals(solution.addDigits(100), 1);
+        Assert.assertEquals(solution.addDigits(123456789), 9);
+        Assert.assertEquals(solution.addDigits(1999999999), 1);
+    }
+
+    @Test
+    public void testPattern() {
+        Assert.assertEquals(solution.addDigitsPattern(1), 1);
+        Assert.assertEquals(solution.addDigitsPattern(11), 2);
+        Assert.assertEquals(solution.addDigitsPattern(99), 9);
+        Assert.assertEquals(solution.addDigitsPattern(100), 1);
+        Assert.assertEquals(solution.addDigitsPattern(123456789), 9);
+        Assert.assertEquals(solution.addDigitsPattern(1999999999), 1);
+    }
+}
