d2ca04c2050f70b167471601bfe9249a46db3fd8
Haizhou Fu <haizhou@uber.com>
2016-10-04 13:09:02 -0700

    single number

---
+++ src/main/java/_136_single_number/Solution.java
@@ -0,0 +1,17 @@
+package _136_single_number;
+
+/**
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+public class Solution {
+    public int singleNumber(int [] nums) {
+        if(nums.length == 0)
+            return 0;
+        int x = nums[0];
+        for(int i = 1; i < nums.length; i++)
+            x ^= nums[i];
+        return x;
+    }
+}
---
+++ src/main/java/_137_single_number_ii/Solution.java
@@ -0,0 +1,39 @@
+package _137_single_number_ii;
+
+/**
+ * Given an array of integers, every element appears three times except for one. Find that single one.
+ * Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+public class Solution {
+    public int singleNumber(int[] nums) {
+
+        // zero i  ii iii
+        // 0    1  0  0
+        // 0    0  1  0
+        int zero = ~0;
+        int i = 0;
+        int ii = 0;
+        int iii = 0;
+
+        for(int j = 0; j < nums.length; j++) {
+            int n = nums[j];
+
+            int carry = n & iii; // carry or not? only 1 & 1 should carry
+            zero ^= carry; // carry
+            iii ^= carry; // after carry  1, 1 --> 0    1, 0 --> 1,   0, 0 --> 0
+
+            carry = n & ii;
+            iii ^= carry;
+            ii ^= carry;
+
+            carry = n & i;
+            ii ^= carry;
+            i ^= carry;
+
+            carry = n & zero;
+            i ^= carry;
+            zero ^= carry;
+        }
+        return i;
+    }
+}
